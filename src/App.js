45285ff2d982f2c54e8598bc37eac7f34c9a83dc
quyenluc22082000 <lucnguyenvq@gmail.com>
2022-03-10 09:40:10 +0700

    fix(update): change ip server images

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
     console.log(parseData)
     // //Download images multi async
     for(let i =0; i < parseData.length; i ++){
-      const response = axios.post('http://35.171.154.180:8443/api/v1/dowloadImages', {
+      const response = axios.post('http://64.227.85.213:8443/api/v1/dowloadImages', {
         "url": parseData[i][1],
         "fileName": parseData[i][0]
       })
